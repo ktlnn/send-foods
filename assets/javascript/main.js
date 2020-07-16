99d7286c20a6361023908e4d06e3ec1a300a95d5
Katelynn <ktlnn.nguyen@gmail.com>
2020-07-15 20:33:22 -0400

    styling cards

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+var apiKey = "1ab12a446aade358252321bfa87f68bc";
 $(document).ready(function () {
     $("#search-button").click(function (event) {
         event.preventDefault();
@@ -12,8 +13,8 @@ function handleSearchParameters() {
     // alert("button clicked");
     // console.log("button clicked");
 
-    var apiKey = "1ab12a446aade358252321bfa87f68bc";
-    // removes all child nodes
+    
+  
 
     // keeps track of search-input value, which is now linked to cityInput
     var cityInput = new URLSearchParams(window.location.search).get('q');
@@ -23,7 +24,7 @@ function handleSearchParameters() {
         url: cityURL,
         method: "GET",
         beforeSend: function (request) {
-            request.setRequestHeader("user-key", "1ab12a446aade358252321bfa87f68bc");
+            request.setRequestHeader("user-key", apiKey);
             // console.log(request);
         }
     }).then(function (response) {
@@ -38,10 +39,11 @@ function handleSearchParameters() {
             url: cuisineURL,
             method: "GET",
             beforeSend: function (request) {
-                request.setRequestHeader("user-key", "1ab12a446aade358252321bfa87f68bc");
+                request.setRequestHeader("user-key", apiKey);
             }
         }).then(function (result) {
             console.log(result);
+            // handleCitySearch(ids here)
             var cuisineID = result.restaurants;
             for (var i = 0; i < cuisineID.length; i++) {
                 console.log(cuisineID[i] + "hello");
@@ -55,9 +57,11 @@ function handleSearchParameters() {
 
                 var results = result.restaurants[i];
 
-                createCardImage.append($("<img>").addClass("card-image").attr("src", results.restaurant.thumb))
+                createCardImage.append($("<p>").addClass("card-title").text(results.restaurant.name));
 
-                createCardImage.append($("<span>").addClass("card-title").text(results.restaurant.name))
+                createCardImage.append($("<img>").addClass("card-image").attr("src", results.restaurant.thumb));
+
+                
 
 
 
@@ -65,10 +69,10 @@ function handleSearchParameters() {
 
                 var resRating = results.restaurant.user_rating.aggregate_rating;
                 
-                createCardContent.append($("<p>").addClass("resRating").html("Restaurant Rating: " + resRating + " ").css('text-align', 'left'));
+                createCardContent.append($("<p>").addClass("res-rating").html("Restaurant Rating: " + resRating + " "));
                 var resAddr = results.restaurant.location.address;
                 var resPhone = results.restaurant.phone_numbers;
-                createCardContent.append($("<p>").addClass("resInfo").html("Address: " + resAddr + '<br>' + "Phone Number: " + resPhone))
+                createCardContent.append($("<p>").addClass("res-info").html("Address: " + resAddr + '<br>' + "Phone Number: " + resPhone));
                 createCard.append(createCardContent);
 
                 var resLink = results.restaurant.url;
@@ -85,4 +89,82 @@ function handleSearchParameters() {
 console.log(window.location.search);
 if (typeof window.location.search != 'undefined' && new URLSearchParams(window.location.search).get('q') != null && new URLSearchParams(window.location.search).get('q').length > 0) {
     handleSearchParameters();
+}
+
+function showMeData(response){
+    console.log(response);
+}
+
+function handleCitySearch(cityID, entityType, callback){
+    var queryUrl = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + cityID + "&entity_type=" + entityType; // <-- inputs go here
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+        beforeSend: function (request) {
+            request.setRequestHeader("user-key", apiKey);
+        }
+    }).then(function(response){
+        // console.log(response);
+        callback(response);
+    });
+}
+// handleCitySearch("36932", "group", showMeData);
+
+function handleCities(search, callback){
+    var queryUrl = "https://developers.zomato.com/api/v2.1/cities?q=" + search; // <-- inputs go here
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+        beforeSend: function (request) {
+            request.setRequestHeader("user-key", apiKey);
+        }
+    }).then(function(response){
+        // console.log(response);
+        callback(response);
+    });
+}
+// handleCities("New York", showMeData);
+
+
+function handleLocations(search, callback){
+    var queryUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + search; // <-- inputs go here
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+        beforeSend: function (request) {
+            request.setRequestHeader("user-key", apiKey);
+        }
+    }).then(function(response){
+        // console.log(response);
+        callback(response);
+    });
+}
+function handleCuisines(cityID, callback){
+    var queryUrl = "https://developers.zomato.com/api/v2.1/search?&count=6&entity_id=" + cityID + "&entity_type=city"; // <-- inputs go here
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+        beforeSend: function (request) {
+            request.setRequestHeader("user-key", apiKey);
+        }
+    }).then(function(response){
+        // console.log(response);
+        callback(response);
+    });
+}
+handleLocations("New York", function(response){
+    // console.log("My custom callback");
+    // console.log(response);
+    handleCuisines(response.location_suggestions[0].entity_id,
+        function(cuisineResult){
+            // console.log("my city")
+            // console.log(cityResult);
+            displayStuff(response, cuisineResult);
+        }
+    )
+});
+
+function displayStuff(locationResponse, cuisineResult){
+    console.log({locationResponse, cuisineResult});
+
 }
